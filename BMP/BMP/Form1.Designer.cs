481722fb59cdd3a52744a49084b4cfa0e75aa45f
Checoffix <checoff@gmail.com>
2023-05-15 23:21:48 +0700

    main

---
+++ BMP/BMP/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Компутэрка_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(895, 564);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(913, 28);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(913, 111);
+            this.trackBar2.Maximum = 15;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(910, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вращение по оси OX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(910, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Вращение по оси OY";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(939, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 603);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BMP/BMP/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Компутэрка_4
+{
+    public partial class Form1 : Form
+    {
+        double[,] Default_Arr;
+        float[,] Current_Arr;
+        readonly Graphics g;
+        int Rotate_X, Rotate_Y;
+        int size;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Rotate_X = trackBar1.Value * 6;
+            Rotate_BMP();
+            Draw_BMP();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Rotate_Y = trackBar2.Value * 6;
+            Rotate_BMP();
+            Draw_BMP();
+        }
+
+        public void Load_BMP()
+        {
+            g.Clear(Color.Black);
+            Bitmap bmp = new Bitmap("circles.bmp");
+            size = bmp.Width;
+            Default_Arr = new double[size, size];
+            Current_Arr = new float[pictureBox1.Width, pictureBox1.Width];
+            g.TranslateTransform(0, pictureBox1.Height);
+            g.ScaleTransform(1, -1);
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    Default_Arr[j, i] = bmp.GetPixel(j, i).GetBrightness() * 100;
+                }
+            }
+            for (int i = 0; i < 1; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (j + 1 < size)
+                    {
+                        g.DrawLine(new Pen(Color.Yellow), j, (float)(Default_Arr[j, i]), j + 1, (float)(Default_Arr[j + 1, i]));
+                    }
+                }
+            }
+        }
+        public void Rotate_BMP()
+        {
+            double Cos_x = Math.Cos(Rotate_X * Math.PI / 180);
+            double Sin_x = Math.Sin(Rotate_X * Math.PI / 180);
+            double Cos_y = Math.Cos(Rotate_Y * Math.PI / 180);
+            double Sin_y = Math.Sin(Rotate_Y * Math.PI / 180);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < pictureBox1.Width; j++)
+                {
+                    Current_Arr[j, i] = (float)(Sin_x * i);
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    float new_X = (float)(Cos_y * j - Sin_y * i + (size / 2 - (Cos_y * size / 2 - Sin_y * size / 2)) + 180);
+                    float new_Z = (float)(Sin_x * i + Cos_x * Default_Arr[j, i]);
+                    if (new_X < pictureBox1.Width && new_X >= 0)
+                    {
+                        Current_Arr[(int)new_X, i] = new_Z;
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Load_BMP();
+        }
+
+        public void Draw_BMP()
+        {
+            g.Clear(Color.Black);
+            float[] lower_z = new float[pictureBox1.Width], high_z = new float[pictureBox1.Width];
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                high_z[i] = Current_Arr[i, 0];
+                lower_z[i] = Current_Arr[i, 0];
+            }
+            for (int i = 0; i < size; i += 10)
+            {
+                for (int j = 0; j < pictureBox1.Width; j += 2)
+                {
+                    if (j + 1 < pictureBox1.Width)
+                    {
+                        if (Current_Arr[j, i] >= high_z[j])
+                        {
+                            if (Current_Arr[j + 1, i] >= high_z[j + 1])
+                            {
+                                g.DrawLine(new Pen(Color.Yellow), j, Current_Arr[j, i], j + 1, Current_Arr[j + 1, i]);
+                                high_z[j] = Current_Arr[j, i];
+                                high_z[j + 1] = Current_Arr[j + 1, i];
+                            }
+                            if (Current_Arr[j + 1, i] <= lower_z[j + 1])
+                            {
+                                g.DrawLine(new Pen(Color.Yellow), j, Current_Arr[j, i], j + 1, Current_Arr[j + 1, i]);
+                                high_z[j] = Current_Arr[j, i];
+                                lower_z[j + 1] = Current_Arr[j + 1, i];
+                            }
+                        }
+                        else if (Current_Arr[j, i] <= lower_z[j])
+                        {
+                            if (Current_Arr[j + 1, i] >= high_z[j + 1])
+                            {
+                                g.DrawLine(new Pen(Color.Yellow), j, Current_Arr[j, i], j + 1, Current_Arr[j + 1, i]);
+                                lower_z[j] = Current_Arr[j, i];
+                                high_z[j + 1] = Current_Arr[j + 1, i];
+                            }
+                            if (Current_Arr[j + 1, i] <= lower_z[j + 1])
+                            {
+                                g.DrawLine(new Pen(Color.Yellow), j, Current_Arr[j, i], j + 1, Current_Arr[j + 1, i]);
+                                lower_z[j] = Current_Arr[j, i];
+                                lower_z[j + 1] = Current_Arr[j + 1, i];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
